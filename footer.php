e9f11cb5f4319ce47a7da49253a46063fc8e97e0
kopplr <liam.r.kopp@gmail.com>
2016-02-03 16:27:51 -0500

    Fonts and some colours correct

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
                 </nav>
                 <div id="mcmaster-footer">
                     <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?> </p>
-                    <img src="images/mcmaster_logo.jpg" alt="McMaster Logo" style="width:497px;height:272px">
+                    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/mcmaster_logo.jpg" alt="McMaster Logo" style="width:250px;height:136px">
                 </div>
 
             </footer><!-- /site-footer -->
