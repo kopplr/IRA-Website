e12ffa193dda0f2aafe2db5e8881500890e560fb
kopplr <liam.r.kopp@gmail.com>
2016-02-17 10:14:12 -0500

    Dropdown menu is functional with filtering

--- js/test_isotope.js
+++ js/test_isotope.js
@@ -2,7 +2,12 @@
 Version: 1.0
 */
 
+
+
 jQuery(document).ready(function($) {
+
+    // add is-checked to the "Show All" button
+    $('#menu-sidebar-menu-links li').first().children().addClass('is-checked');
 
     // init Isotope
     var $grid = $('.grid').isotope({
@@ -13,17 +18,43 @@ jQuery(document).ready(function($) {
     // filter items on button click
     $('.filter-button-group').on( 'click', 'button', function() {
       var filterValue = $(this).attr('data-filter');
-        console.log($grid);
       $grid.isotope({ filter: filterValue });
     });
 
+
+
+
+    // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+
+        var $isSubMenu = $buttonGroup.find('.is-checked').parents('.sub-menu');
+        if($isSubMenu.exists() && !$(this).parents('.sub-menu').exists()){ // Selected a button filter that is NOT in the sub-menu, so slideUp
+            $isSubMenu.slideUp(200);
+        }
+
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+
     //Dropdown menu
-    if ( $('.button-group li').children().length > 0){
-        $('.button-group li').hover(function(){
-            $(this).children('ul').css({display: 'block'});
-        }, function(){
-            $(this).children('ul').css({display: 'none'});
-        });
-    };
+    $('.menu-item-has-children').hover(
+        function(){
+            $(this).children('.sub-menu').slideDown(200);
+        },
+        function(){
+            if(!$(this).find('.is-checked').exists()) { // If a submenu button is NOT checked, then slideUp
+                $(this).children('.sub-menu').slideUp(200);
+            }
+        }
+    );
+
+    // Extra jQuery functions
+    $.fn.exists = function () {
+        return this.length !== 0;
+    }
+
 
 });
